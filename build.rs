d700f8537c700b9e56c1089b6adb6cdfb4e73c6c
4513ECHO <mail@4513echo.dev>
2023-09-17 03:37:28 +0900

    test: Fix build.rs for older protoc

--- build.rs
+++ build.rs
@@ -1,5 +1,14 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["src/codegen.proto"], &["src/"]).unwrap();
+    let mut prost_build = prost_build::Config::new();
+    // NOTE: This flag is required because the feature is enabled by default since protoc v3.15.0,
+    // but protoc in the `ubuntu-latest` image of GitHub Actions is v3.12.4.
+    // References:
+    // https://github.com/protocolbuffers/protobuf/releases/tag/v3.15.0
+    // https://packages.ubuntu.com/jammy/protobuf-compiler
+    prost_build.protoc_arg("--experimental_allow_proto3_optional");
+    prost_build
+        .compile_protos(&["src/codegen.proto"], &["src/"])
+        .unwrap();
 }
